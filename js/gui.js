e33ab7c8403526f086ee6141b41cbea6675a72d9
withmorten <morten.with@gmail.com>
2017-12-10 17:53:11 +0100

    cleanup

--- js/gui.js
+++ js/gui.js
@@ -24,61 +24,61 @@ function initGui() {
     timeForm.attr("method", "post");
     timeForm.attr("enctype", "multipart/form-data");
     timeForm.attr("id", timeFormId);
-    
+
     var maxFileSizeInput = $("<input>");
     maxFileSizeInput.attr("type", "hidden");
     maxFileSizeInput.attr("name", "max_file_size");
     maxFileSizeInput.attr("value", "2097152");
     timeForm.append(maxFileSizeInput);
-    
+
     var timeTable = $("<table>");
     timeTable.attr("id", timeTableId);
-    
+
     var tHeaderRow = $("<tr>");
     tHeaderRow.attr("id", theaderId);
-    
+
     tHeaderRow.append($("<td>"));
-    
+
     var tHeaderField = $("<th>");
     tHeaderField.attr("colspan", "3");
     tHeaderField.text("resyncs subs via timings");
     tHeaderRow.append(tHeaderField);
     tHeaderRow.append(plusOneButton().click(addTimeLine));
-    
+
     timeTable.append(tHeaderRow);
-    
+
     var fileRow = $("<tr>");
     fileRow.attr("id", fileRowId);
     fileRow.append($("<td>"));
-    
+
     var fileField = $("<td>");
     fileField.attr("colspan", "4");
-    
+
     var fileInput = $("<input>");
     fileInput.attr("type", "file");
     fileInput.attr("accept", ".srt");
     fileInput.attr("name", "srtfiles[]");
     fileInput.attr("multiple", "multiple");
-    
+
     fileField.append(fileInput);
     fileRow.append(fileField);
-    
+
     timeTable.append(fileRow);
-    
+
     var tSubmit = $("<td>");
     var inputSubmit = $("<input>");
     inputSubmit.attr("name", "submit");
     inputSubmit.attr("type", "submit");
     inputSubmit.attr("value", "resync");
     tSubmit.append(inputSubmit);
-    
+
     var tReset = $("<td>");
     var inputReset = $("<input>");
     inputReset.attr("type", "reset");
     inputReset.attr("value", "reset");
     inputReset.attr("onclick", "return confirm('Really?');");
     tReset.append(inputReset);
-    
+
     var tFooterRow = $("<tr>");
     tFooterRow.attr("id", tfooterId);
     tFooterRow.append($("<td>").attr("colspan", "1"));
@@ -86,44 +86,44 @@ function initGui() {
     tFooterRow.append($("<td>"));
     tFooterRow.append(tReset);
     tFooterRow.append($("<td>"));
-    
+
     timeTable.append(tFooterRow);
     timeForm.append(timeTable);
-    
+
     var textArea = $("<textarea>");
     textArea.attr("id", "jsonarea");
     textArea.attr("name", "jsonarea");
     textArea.val($("#phparea").val());
-    
+
     timeForm.append(textArea);
-    
+
     $("body").append(timeForm);
 }
 
 function addTimeLine(event = false) {
     // gets timeTable
     var timeFormTable = $("#"+timeTableId);
-    
+
     // initialising new table row
     var timeLine = $("<tr>");
     timeLine.addClass(timeLineClassId);
-    
+
     // td for timeline Number in front
     var timeLineNumberTd = $("<td>");
     timeLineNumberTd.addClass(timeLineNumberClassId);
     timeLineNumberTd.addClass(timeFormId);
     timeLine.append(timeLineNumberTd);
-    
+
     // add the timings input fields
     $.each(timeInputTypes, function(i, timeInputType) {
         var timeInputTd = $("<td>");
         var timeInput = $("<input>");
-        
+
         timeInput.attr("type", "text");
 
         timeInput.addClass(timeClassId);
         timeInput.addClass(timeFormId);
-        
+
         if(timeInputType === "sign") {
             timeInput.attr("value", defaultTimeSign);
             timeInput.attr("readonly", "readonly");
@@ -132,19 +132,19 @@ function addTimeLine(event = false) {
         } else {
             timeInput.attr("placeholder", timePlaceHolder);
             timeInput.attr("value", timePlaceHolder);
-            
+
             // focus off AFTER so it doesn't get selected onclick
             timeInput.mask(timeMask);
             timeInput.off('focus');
         }
-        
+
         timeInputTd.append(timeInput);
         timeLine.append(timeInputTd);
     });
-    
+
     // + button
     timeLine.append(plusOneButton().click(addTimeLine));
-    
+
     // depending on whether it was created by default or by click,
     // append the timeLine at the bottom or below the clicked one
     if(event !== false) {
@@ -153,7 +153,7 @@ function addTimeLine(event = false) {
     } else {
         $('#'+theaderId).after(timeLine);
     }
-    
+
     // number all the timelines properly, especially after adding one inbetween
     var timeLines = timeFormTable.find("."+timeLineClassId);
     $.each(timeLines, function(i, timeLine) {
@@ -161,9 +161,9 @@ function addTimeLine(event = false) {
         var timeLineNum = i+1;
         timeLine.attr("id", timeLineClassId+timeLineNum);
         $(timeLine.find('td')[0]).text(timeLineNum);
-        
+
         var timeInputs = timeLine.find('input');
-        
+
         $.each(timeInputTypes, function(j, timeInputType) {
             $(timeInputs[j]).attr("name", timeLineClassId+"s["+i+"]["+timeInputType+"]");
         });
@@ -179,13 +179,13 @@ function toggleSignage(event) {
 function plusOneButton() {
     var plusOneTd = $("<td>");
     var plusOneButton = $("<input>");
-    
+
     plusOneButton.attr("type", "button");
     plusOneButton.attr("value", "+");
-    
+
     plusOneButton.addClass(timeFormId);
     plusOneButton.addClass(timePlusOneId);
-    
+
     return plusOneTd.append(plusOneButton);
 }
 
@@ -195,6 +195,6 @@ function getDefaultSign() {
     } else {
         var sign = "-";
     }
-    
+
     return sign;
 }
